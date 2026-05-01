eaa687c9303394f271c4e44e542d558a5da5d1af
Saumel-Das <saumel.spr@gmail.com>
2026-05-01 21:18:33 +0600

    necessary packages installed

---
+++ librova/src/app/(main)/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "LibRova | Home",
+  description: "Borrow, lend, and discover your next favorite read. LibRova connects book lovers to share stories and build a sustainable local library",
+};
+
+export default function Home() {
+  return (
+    <div>
+      HomePage
+    </div>
+  );
+}
